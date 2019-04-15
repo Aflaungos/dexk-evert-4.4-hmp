7da743dd356e373fb7659d3ad2e4f1e378a96d55
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-14 22:04:53 -0700

    msm: mdss: Power on display asynchronously as early as possible
    Currently, mdss powers on the display a long time after an unblank is
    requested and gets completely blocked while powering on the display, so
    if the display takes a very long time to turn on, then mdss will be
    stuck unable to do anything else in the meantime. This results in a long
    delay between trying to wake the device and the display actually
    powering on.
    
    In order to make the display turn on faster when waking the device from
    sleep, start powering on the display as soon as the framebuffer unblank
    event is received. This allows mdss to continue resuming while the
    display takes its time powering on. A high-priority kthread is used here
    to ensure the display powers on as quickly as possible.
    
    In the event that the framebuffer unblank notifier is not used (such as
    for AOD), the display will be powered on at the time that it is
    requested via the MDSS_EVENT_LINK_READY event.
    
    To make this work, kickoffs need to be blocked when they attempt to
    power on the display, so that a kickoff won't continue while the display
    is still powered off.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- drivers/video/fbdev/msm/mdss_dsi.c
+++ drivers/video/fbdev/msm/mdss_dsi.c
@@ -1883,6 +1883,7 @@ static irqreturn_t test_hw_vsync_handler(int irq, void *data)
 
 static int mdss_dsi_disp_wake_thread(void *data)
 {
+<<<<<<< HEAD
 	struct mdss_panel_data *pdata = data;
 	struct mdss_dsi_ctrl_pdata *ctrl_pdata =
 		container_of(pdata, typeof(*ctrl_pdata), panel_data);
@@ -1924,10 +1925,50 @@ static void mdss_dsi_start_wake_thread(struct mdss_dsi_ctrl_pdata *ctrl_pdata)
 				__func__, PTR_ERR(ctrl_pdata->wake_thread));
 		ctrl_pdata->wake_thread = NULL;
 	}
+=======
+	static const struct sched_param max_rt_param = {
+		.sched_priority = MAX_RT_PRIO - 1
+	};
+	struct mdss_dsi_ctrl_pdata *ctrl_pdata = data;
+	struct mdss_panel_data *pdata = &ctrl_pdata->panel_data;
+
+	sched_setscheduler_nocheck(current, SCHED_FIFO, &max_rt_param);
+
+	while (1) {
+		bool should_stop;
+
+		wait_event(ctrl_pdata->wake_waitq,
+			(should_stop = kthread_should_stop()) ||
+			atomic_cmpxchg(&ctrl_pdata->disp_en,
+				       MDSS_DISPLAY_WAKING,
+				       MDSS_DISPLAY_ON) == MDSS_DISPLAY_WAKING);
+
+		if (should_stop)
+			break;
+
+		/* MDSS_EVENT_LINK_READY */
+		if (ctrl_pdata->refresh_clk_rate)
+			mdss_dsi_clk_refresh(pdata,
+					     ctrl_pdata->update_phy_timing);
+		mdss_dsi_on(pdata);
+
+		/* MDSS_EVENT_UNBLANK */
+		mdss_dsi_unblank(pdata);
+
+		/* MDSS_EVENT_PANEL_ON */
+		ctrl_pdata->ctrl_state |= CTRL_STATE_MDP_ACTIVE;
+		pdata->panel_info.esd_rdy = true;
+
+		complete_all(&ctrl_pdata->wake_comp);
+	}
+
+	return 0;
+>>>>>>> 25fa4e7ad822 (msm: mdss: Power on display asynchronously as early as possible)
 }
 
 static void mdss_dsi_display_wake(struct mdss_dsi_ctrl_pdata *ctrl_pdata)
 {
+<<<<<<< HEAD
 	if (atomic_read(&ctrl_pdata->disp_is_on))
 		return;
 
@@ -1958,6 +1999,23 @@ static int mdss_dsi_fb_unblank_cb(struct notifier_block *nb,
 		mdss_dsi_display_wake(ctrl_pdata);
 	else
 		mdss_dsi_start_wake_thread(ctrl_pdata);
+=======
+	if (atomic_cmpxchg(&ctrl_pdata->disp_en, MDSS_DISPLAY_OFF,
+			   MDSS_DISPLAY_WAKING) == MDSS_DISPLAY_OFF)
+		wake_up(&ctrl_pdata->wake_waitq);
+}
+
+static int mdss_dsi_fb_unblank_cb(struct notifier_block *nb,
+				  unsigned long action, void *data)
+{
+	struct mdss_dsi_ctrl_pdata *ctrl_pdata =
+		container_of(nb, typeof(*ctrl_pdata), wake_notif);
+	int *blank = ((struct fb_event *)data)->data;
+
+	/* Parse unblank events as soon as they occur */
+	if (action == FB_EARLY_EVENT_BLANK && *blank == FB_BLANK_UNBLANK)
+		mdss_dsi_display_wake(ctrl_pdata);
+>>>>>>> 25fa4e7ad822 (msm: mdss: Power on display asynchronously as early as possible)
 
 	return NOTIFY_OK;
 }
@@ -3099,7 +3157,12 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->off_cmds.link_state == DSI_LP_MODE)
 			rc = mdss_dsi_blank(pdata, power_state);
 		rc = mdss_dsi_off(pdata, power_state);
+<<<<<<< HEAD
 		atomic_set(&ctrl_pdata->disp_is_on, 0);
+=======
+		reinit_completion(&ctrl_pdata->wake_comp);
+		atomic_set(&ctrl_pdata->disp_en, MDSS_DISPLAY_OFF);
+>>>>>>> 25fa4e7ad822 (msm: mdss: Power on display asynchronously as early as possible)
 		break;
 	case MDSS_EVENT_DISABLE_PANEL:
 		/* disable esd thread */
@@ -3892,8 +3955,25 @@ static int mdss_dsi_ctrl_probe(struct platform_device *pdev)
 
 	mdss_dsi_debug_bus_init(mdss_dsi_res);
 
+<<<<<<< HEAD
 	ctrl_pdata->wake_notif.notifier_call = mdss_dsi_fb_unblank_cb;
 	ctrl_pdata->wake_notif.priority = INT_MAX - 1;
+=======
+	init_completion(&ctrl_pdata->wake_comp);
+	init_waitqueue_head(&ctrl_pdata->wake_waitq);
+	ctrl_pdata->wake_thread = kthread_run(mdss_dsi_disp_wake_thread,
+					      ctrl_pdata, "mdss_display_wake");
+	if (IS_ERR(ctrl_pdata->wake_thread)) {
+		rc = PTR_ERR(ctrl_pdata->wake_thread);
+		pr_err("%s: Failed to start display wake thread, rc=%d\n",
+		       __func__, rc);
+		goto error_shadow_clk_deinit;
+	}
+
+	/* It's sad but not fatal for the fb client register to fail */
+	ctrl_pdata->wake_notif.notifier_call = mdss_dsi_fb_unblank_cb;
+	ctrl_pdata->wake_notif.priority = INT_MAX;
+>>>>>>> 25fa4e7ad822 (msm: mdss: Power on display asynchronously as early as possible)
 	fb_register_client(&ctrl_pdata->wake_notif);
 
 	return 0;
@@ -4364,6 +4444,10 @@ static int mdss_dsi_ctrl_remove(struct platform_device *pdev)
 	}
 
 	fb_unregister_client(&ctrl_pdata->wake_notif);
+<<<<<<< HEAD
+=======
+	kthread_stop(ctrl_pdata->wake_thread);
+>>>>>>> 25fa4e7ad822 (msm: mdss: Power on display asynchronously as early as possible)
 	mdss_dsi_pm_qos_remove_request(ctrl_pdata->shared_data);
 
 	if (msm_dss_config_vreg(&pdev->dev,
--- drivers/video/fbdev/msm/mdss_dsi.h
+++ drivers/video/fbdev/msm/mdss_dsi.h
@@ -607,8 +607,18 @@ struct mdss_dsi_ctrl_pdata {
 	struct task_struct *wake_thread;
 	struct completion wake_comp;
 	wait_queue_head_t wake_waitq;
+<<<<<<< HEAD
 	atomic_t disp_is_on;
 	atomic_t needs_wake;
+=======
+	atomic_t disp_en;
+};
+
+enum {
+	MDSS_DISPLAY_OFF,
+	MDSS_DISPLAY_WAKING,
+	MDSS_DISPLAY_ON
+>>>>>>> 25fa4e7ad822 (msm: mdss: Power on display asynchronously as early as possible)
 };
 
 struct dsi_status_data {
