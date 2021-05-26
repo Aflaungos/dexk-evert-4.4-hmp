57de83a968045f24d9cc9a6d1e4b5abc9cf7c093
psndna88 <psndna88@gmail.com>
2021-05-26 18:45:07 +0530

    selinux: implement sel_inode_cache file_security_cache from CAF A11 tag

--- include/linux/selinux.h
+++ include/linux/selinux.h
@@ -18,6 +18,31 @@ struct selinux_audit_rule;
 struct audit_context;
 struct kern_ipc_perm;
 
+<<<<<<< HEAD
+=======
+struct file_security_struct {
+	u32 sid;		/* SID of open file description */
+	u32 fown_sid;		/* SID of file owner (for SIGIO) */
+	u32 isid;		/* SID of inode at the time of file open */
+	u32 pseqno;		/* Policy seqno at the time of file open */
+};
+
+struct inode_security_struct {
+	struct inode *inode;	/* back pointer to inode object */
+	union {
+		struct list_head list;	/* list of inode_security_struct */
+		struct rcu_head rcu;	/* for freeing the inode_security_struct */
+	};
+	u32 task_sid;		/* SID of creating task */
+	u32 sid;		/* SID of this object */
+	u16 sclass;		/* security class of this object */
+	unsigned char initialized;	/* initialization flag */
+	u32 tag;		/* Per-File-Encryption tag */
+	void *pfk_data; /* Per-File-Key data from ecryptfs */
+	struct mutex lock;
+};
+
+>>>>>>> c9458dfc41bd (selinux: implement sel_inode_cache file_security_cache from CAF A11 tag)
 #ifdef CONFIG_SECURITY_SELINUX
 
 /**
