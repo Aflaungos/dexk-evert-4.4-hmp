f17bc021dc62244bad7f2d6938a31e01e8cbd2d1
seba3567 <sebastian26481@gmail.com>
2021-05-31 02:49:30 -0400

    Revert "msm: kgsl: Dispatch commands using a master kthread"
    This reverts commit 1c472d5f5351dfe8c3a80f0908cfef3e3a826329.

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -2834,14 +2834,9 @@ int adreno_dispatcher_init(struct adreno_device *adreno_dev)
 
 	init_waitqueue_head(&dispatcher->cmd_waitq);
 	dispatcher->state = (atomic_t)ATOMIC_INIT(THREAD_IDLE);
-<<<<<<< HEAD
 	dispatcher->thread = kthread_run_perf_critical(cpu_perf_mask, adreno_dispatcher_thread,
 						       adreno_dev,
 						       "adreno_dispatch");
-=======
-	dispatcher->thread = kthread_run(adreno_dispatcher_thread, adreno_dev,
-					 "adreno_dispatch");
->>>>>>> 120fa3392ca6 (msm: kgsl: Dispatch commands using a master kthread)
 	if (IS_ERR(dispatcher->thread))
 		return PTR_ERR(dispatcher->thread);
 
