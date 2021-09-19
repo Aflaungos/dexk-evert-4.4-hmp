05d6f8069de54ad9a63caf6eae82a187bbec3431
seba3567 <sebastian26481@gmail.com>
2021-09-19 16:32:43 -0300

    fix build info.h

--- include/linux/thread_info.h
+++ include/linux/thread_info.h
@@ -11,10 +11,7 @@
 #include <linux/bug.h>
 #include <linux/restart_block.h>
 
-
-struct timespec;
-struct compat_timespec;
-
+#ifdef CONFIG_THREAD_INFO_IN_TASK
 /*
  * For CONFIG_THREAD_INFO_IN_TASK kernels we need <asm/current.h> for the
  * definition of current, but for !CONFIG_THREAD_INFO_IN_TASK kernels,
@@ -131,7 +128,6 @@ extern void __check_object_size(const void *ptr, unsigned long n,
 static __always_inline void check_object_size(const void *ptr, unsigned long n,
 					      bool to_user)
 {
-	return;
 	if (!__builtin_constant_p(n))
 		__check_object_size(ptr, n, to_user);
 }
